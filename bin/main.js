8d32bdce1d42c62e07fe1b9eff65f0a6091e15aa
Xiaocheng Wei <xiaocheng.wei@sap.com>
2017-12-11 13:41:04 +0800

    Add server.js for http response

--- bin/main.js
+++ bin/main.js
@@ -1,5 +1,69 @@
 #! /usr/bin/env node
 
-var oKpi = require("../lib/kpi");
+//var Http = require("http");
+var Http = require("https");
+var Kpi = require("../lib/kpi");
+//var ODataModel = require("https://sapui5.hana.ondemand.com/resources/sap/ui/model/odata/v2/ODataModel.js"); 
 
-console.log("Pass case:" + oKpi.kpi.CaseStatus().pass);
+console.log("Pass case:" + Kpi.kpi.CaseStatus().pass);
+
+/* 
+ * Test http request to I7D
+*/
+var fnResponse = function(oResponse){
+	console.log("Status Code: " + oResponse.statusCode + "\n");
+	console.log("Headers: " + oResponse.headers);
+
+	var vBody = [];
+
+	oResponse.on("data", function(oChunk){
+		vBody.push(oChunk);
+	});
+
+	oResponse.on("error", function(oError){
+		console.error(oError);
+	});
+
+	oResponse.on("end", function(){
+		vBody = Buffer.concat(vBody);
+		console.log(vBody.toString());
+	});
+};
+
+//Way 1
+var options = {
+	hostname: "i7d.wdf.sap.corp",
+	port: 443,
+	path: "/odata/sbou/ContacterSet('0006269256')",
+	method: "GET",
+	rejectUnauthorized: false,
+	headers: {
+		"Content-Type": "application/http"
+	}
+}; 
+
+var oRequest = Http.request(options, fnResponse);
+
+//oRequest.write("request body data");
+
+oRequest.on("error", function(oError){
+	console.error(oError);
+});
+
+oRequest.end();
+
+//Way 2
+//Http.get("https://i7d.wdf.sap.corp:443/odata/sbou/ContacterSet('0006269256')", fnResponse);
+
+
+//Way 3
+/*var oDataMode = new ODataModel("https://i7d.wdf.sap.corp/odata/sbou");
+console.log(oDataMode.getMetadata().getName());*/
+
+//Way 4
+/*$.ajax({
+	url: "https://i7d.wdf.sap.corp/odata/sbou/ContacterSet('0006269256')",
+	success: function(){
+		console.log("Hello");
+	},
+});*/
