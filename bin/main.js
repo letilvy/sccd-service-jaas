a639303567aece8fc884298bb6953ee252ae63e6
Xiaocheng Wei <xiaocheng.wei@sap.com>
2018-01-23 09:26:48 +0800

    Add CLI command option

--- bin/main.js
+++ bin/main.js
@@ -1,49 +1,32 @@
 #! /usr/bin/env node
 
-/***
-	Run the program in CLI like:
-	  node main.js jenkins_job_workspace_path branch_name
-	Or
-	  sccd jenkins_job_workspace_path branch_name //on jenkins server
-	You can get the first CLI parameter through process.argv[2]
-*/
-
 var Project = require("../lib/project");
 var DB = require("../lib/db");
 var Argv = require("optimist").boolean("cors").argv;
 
+/***
+	Run the program in CLI like:
+	  node main.js -p <jenkins_job_workspace_path> -b <branch_name>
+	Or
+	  node main.js <jenkins_job_workspace_path> <branch_name>
+	Or
+	  sccd <jenkins_job_workspace_path> <branch_name> //on jenkins server
+	Or
+	  sccd -p <jenkins_job_workspace_path> -b <branch_name> //on jenkins server
+	You can get the first CLI parameter through process.argv[2]
+*/
 if (Argv.h || Argv.help) {
   	console.log([
-	    'usage: main [path] [options]',
+  		'',
+	    'usage: node main.js [options]',
 	    '',
 	    'options:',
-	    '  -p           Port to use [8080]',
-	    '  -a           Address to use [0.0.0.0]',
-	    '  -d           Show directory listings [true]',
-	    '  -i           Display autoIndex [true]',
-	    '  -g --gzip    Serve gzip files when possible [false]',
-	    '  -e --ext     Default file extension if none supplied [none]',
-	    '  -s --silent  Suppress log messages from output',
-	    '  --cors[=headers]   Enable CORS via the "Access-Control-Allow-Origin" header',
-	    '                     Optionally provide CORS headers list separated by commas',
-	    '  -o [path]    Open browser window after starting the server',
-	    '  -c           Cache time (max-age) in seconds [3600], e.g. -c10 for 10 seconds.',
-	    '               To disable caching, use -c-1.',
-	    '  -U --utc     Use UTC time format in log messages.',
-	    '',
-	    '  -P --proxy   Fallback proxy if the request cannot be resolved. e.g.: http://someurl.com',
-	    '',
-	    '  -S --ssl     Enable https.',
-	    '  -C --cert    Path to ssl cert file (default: cert.pem).',
-	    '  -K --key     Path to ssl key file (default: key.pem).',
-	    '',
-	    '  -r --robots  Respond to /robots.txt [User-agent: *\\nDisallow: /]',
+	    '  -p           Jenkins job workspace path. e.g.:/var/lib/jenkins/workspace/B1_SMP_PUM',
+	    '  -b           Branch which test case run on [master]',
 	    '  -h --help    Print this list and exit.'
   	].join('\n'));
   	process.exit();
 }
-
-var aArgv = process.argv.slice(2);
 
 function getTimestamp(oDate){
 	var sYear = oDate.getFullYear();
@@ -62,8 +45,10 @@ function getTimestamp(oDate){
 	return String(sYear) + String(sMonth) + String(sDay) + String(sHour) + String(sMin) + String(sSec);
 }
 
+var aArgv = process.argv.slice(2);
+
 var oProject = new Project({
-	workSpace: aArgv[0] || "./" //Use "../data/B1 SMP PUM" as debug purpose
+	workSpace: Argv.p || aArgv[0] || "./" //Use "../data/B1 SMP PUM" as debug purpose
 });
 var oDB = new DB({
 	name: "sccd"
@@ -71,7 +56,11 @@ var oDB = new DB({
 
 //${GIT_BRANCH} has a prefix "origin/", while ${GERRIT_BRANCH} does not
 var sBranch = "master";
-if(aArgv[1] && aArgv[1].match(/^.*\/(\w+)$/)){
+if(Argv.b && Argv.b.match(/^.*\/(\w+)$/)){
+	sBranch = (Argv.b.match(/^.*\/(\w+)$/))[1];
+}else if(Argv.b){
+	sBranch = Argv.b;
+}else if(aArgv[1] && aArgv[1].match(/^.*\/(\w+)$/)){
 	sBranch = (aArgv[1].match(/^.*\/(\w+)$/))[1];
 }else if(aArgv[1]){
 	sBranch = aArgv[1];
