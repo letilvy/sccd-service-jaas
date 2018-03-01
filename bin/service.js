f9f60139990171e76f65ca4483cfd22ebd193ea9
Wanying Xie <wanying.xie@sap.com>
2018-03-01 18:05:43 +0800

    add ITSet query

--- bin/service.js
+++ bin/service.js
@@ -33,17 +33,92 @@ var oServer = HTTP.createServer(function(request, response){
         oDB.connect();
 
         //projectId
-
+        
         //HomeSet: list all test cases of UT and IT
-        /*var queryStr = "";
+        /*
+        select 
+            ut.projectId,
+            ut.projectName,
+            ut.teamId,
+            ut.teamName,
+            ut.passed+ut.failed totalUT,
+            ut.assertion UTAssertion,
+            it.passed+ut.failed totalIT,
+            it.assertion ITAssertion
+        from
+                (select 
+                    ut.tcid tcid,
+                    ut.pid projectId,
+                    p.name projectName,
+                    p.team teamId,
+                    t.name teamName,
+                    ut.passed passed,
+                    ut.failed failed,
+                    ut.assertion assertion,
+                    ut.branch branch,
+                    ut.timestamp timestamp
+                from
+                    (select 
+                        t.pid, 
+                        substring_index(group_concat(t.tcid), ',', -1) last_commit_id
+                    from 
+                        (select 
+                            u.tcid,
+                            u.pid,
+                            u.timestamp
+                        from UT u
+                        where timestamp > 0
+                        and branch = 'master'
+                        order by u.pid asc, timestamp desc) t group by t.pid) l
+                left join UT ut
+                on l.last_commit_id = ut.tcid
+                left join Project p
+                on ut.pid = p.pid
+                left join Team t
+                on p.team = t.tid) ut
+        left join
+                (select 
+                    it.tcid tcid,
+                    it.pid projectId,
+                    p.name projectName,
+                    p.team teamId,
+                    t.name teamName,
+                    it.passed passed,
+                    it.failed failed,
+                    it.assertion assertion,
+                    it.branch branch,
+                    it.timestamp timestamp
+                from
+                    (select 
+                        i.pid, 
+                        substring_index(group_concat(i.tcid), ',', -1) last_commit_id
+                    from 
+                        (select 
+                            i.tcid,
+                            i.pid,
+                            i.timestamp
+                        from IT i
+                        where timestamp > 0
+                        and branch = 'master'
+                        order by i.pid asc, i.timestamp desc) i group by i.pid) l
+                left join IT it
+                on l.last_commit_id = it.tcid
+                left join Project p
+                on it.pid = p.pid
+                left join Team t
+                on p.team = t.tid) it
+        on ut.projectId = it.projectId
+        order by  ut.teamId desc, ut.assertion desc, it.assertion desc
+        */
+        var sHomeQuery = "select ut.projectId, ut.projectName, ut.teamId, ut.teamName, ut.passed+ut.failed totalUT, ut.assertion UTAssertion, it.passed+ut.failed totalIT, it.assertion ITAssertion from (select ut.tcid tcid, ut.pid projectId, p.name projectName, p.team teamId, t.name teamName, ut.passed passed, ut.failed failed, ut.assertion assertion, ut.branch branch, ut.timestamp timestamp from (select t.pid, substring_index(group_concat(t.tcid), ',', -1) last_commit_id from (select u.tcid, u.pid, u.timestamp  from UT u  where timestamp > 0  and branch = 'master'  order by u.pid asc, timestamp desc) t group by t.pid) l left join UT ut on l.last_commit_id = ut.tcid left join Project p on ut.pid = p.pid left join Team t on p.team = t.tid) ut left join (select it.tcid tcid, it.pid projectId, p.name projectName, p.team teamId, t.name teamName, it.passed passed, it.failed failed, it.assertion assertion, it.branch branch, it.timestamp timestamp from (select i.pid, substring_index(group_concat(i.tcid), ',', -1) last_commit_id from (select i.tcid, i.pid, i.timestamp  from IT i  where timestamp > 0  and branch = 'master'  order by i.pid asc, i.timestamp desc) i group by i.pid) l left join IT it on l.last_commit_id = it.tcid left join Project p on it.pid = p.pid left join Team t on p.team = t.tid) it on ut.projectId = it.projectId order by ut.teamId desc, ut.assertion desc, it.assertion desc";
        
-        oDB.query(queryStr, function(oError, aResult){
+        oDB.query(sHomeQuery, function(oError, aResult){
             if(oError){
                 console.log("[Database error] - ", oError.message); //write header instead
                 return;
             }
 
-            if(sUrl.match("UITSet")){
+            if(sUrl.match("HomeSet")){
                 response.end(JSON.stringify(aResult));
             }else{
                 response.end(JSON.stringify({
@@ -51,63 +126,43 @@ var oServer = HTTP.createServer(function(request, response){
                     "key2": "SAP-IUser"
                 }));
             }
-        });*/
+        });
 
         //UTSet: list the last commit UT data of all project
         /*
-          select
-                ut.tcid,
-                ut.pid,
-                ut.name,
-                ut.team,
-                t.name,
-                ut.passed,
-                ut.failed,
-                ut.assertion,
-                ut.branch,
-                ut.timestamp
+            select 
+                ut.tcid tcid,
+                ut.pid projectId,
+                p.name projectName,
+                p.team teamId,
+                t.name teamName,
+                ut.passed passed,
+                ut.failed failed,
+                ut.assertion assertion,
+                ut.branch branch,
+                ut.timestamp timestamp
             from
                 (select 
-                    ut.tcid,
-                    ut.pid,
-                    p.name,
-                    p.team,
-                    ut.passed,
-                    ut.failed,
-                    ut.assertion,
-                    ut.branch,
-                    ut.timestamp
-                from
+                    t.pid, 
+                    substring_index(group_concat(t.tcid), ',', -1) last_commit_id
+                from 
                     (select 
-                        ut.tcid,
-                        ut.pid,
-                        ut.passed,
-                        ut.failed,
-                        ut.assertion,
-                        ut.branch,
-                        ut.timestamp
-                    from
-                        (select 
-                            t.pid, 
-                            substring_index(group_concat(t.tcid), ',', -1) last_commit_id
-                        from 
-                            (select 
-                                u.tcid,
-                                u.pid,
-                                u.timestamp
-                            from UT u
-                            where timestamp > 0
-                            and branch = 'master'
-                            order by u.pid asc, timestamp desc) t group by t.pid) l
-                    left join UT ut
-                    on l.last_commit_id = ut.tcid) ut
-                left join Project p
-                on ut.pid = p.pid) ut
+                        u.tcid,
+                        u.pid,
+                        u.timestamp
+                    from UT u
+                    where timestamp > 0
+                    and branch = 'master'
+                    order by u.pid asc, timestamp desc) t group by t.pid) l
+            left join UT ut
+            on l.last_commit_id = ut.tcid
+            left join Project p
+            on ut.pid = p.pid
             left join Team t
-            on ut.team = t.tid
-            order by ut.team, ut.assertion desc
+            on p.team = t.tid
+            order by p.team desc, ut.assertion desc
         */
-        var sUTQuery = "select ut.tcid tcid, ut.pid projectId, ut.name projectName, ut.team teamId, t.name teamName, ut.passed passed, ut.failed failed, ut.assertion assertion, ut.branch branch, ut.timestamp timestamp from (select ut.tcid, ut.pid, p.name, p.team, ut.passed, ut.failed, ut.assertion, ut.branch, ut.timestamp from (select ut.tcid, ut.pid, ut.passed, ut.failed, ut.assertion, ut.branch, ut.timestamp from (select t.pid, substring_index(group_concat(t.tcid), ',', -1) last_commit_id from (select u.tcid, u.pid, u.timestamp from UT u where timestamp > 0 and branch = 'master' order by u.pid asc, timestamp desc) t group by t.pid) l left join UT ut on l.last_commit_id = ut.tcid) ut left join Project p on ut.pid = p.pid) ut left join Team t on ut.team = t.tid order by ut.team, ut.assertion desc";
+        var sUTQuery = "select ut.tcid tcid, ut.pid projectId, p.name projectName, p.team teamId, t.name teamName, ut.passed passed, ut.failed failed, ut.assertion assertion, ut.branch branch, ut.timestamp timestamp from (select t.pid, substring_index(group_concat(t.tcid), ',', -1) last_commit_id from (select u.tcid, u.pid, u.timestamp from UT u where timestamp > 0 and branch = 'master' order by u.pid asc, timestamp desc) t group by t.pid) l left join UT ut on l.last_commit_id = ut.tcid left join Project p on ut.pid = p.pid left join Team t on p.team = t.tid order by p.team desc, ut.assertion desc";
         oDB.query(sUTQuery, function(oError, aResult){
             if(oError){
                 console.log("[Database error] - ", oError.message); //write header instead
@@ -125,7 +180,28 @@ var oServer = HTTP.createServer(function(request, response){
         });
 
         //ProjectDetailUTSet: list history UT data with specified project
-        oDB.query("select u.tcid, p.name, u.passed, u.failed, u.assertion, u.skipped, u.timestamp from Project p, UT u where p.pid = u.pid and p.pid='sap.support.expertchat' order by timestamp;", function(oError, aResult){
+        /*
+            select 
+                ut.tcid tcid,
+                ut.pid projectId,
+                p.name projectName,
+                ut.passed passed,
+                ut.failed failed,
+                ut.assertion assertion,
+                p.team teamId,
+                t.name teamName,
+                ut.timestamp timestamp
+            from UT ut
+            left join Project p
+            on (ut.pid = p.pid )
+            left join Team t
+            on (p.team = t.tid)
+            where ut.pid = 'sap.support.sae' 
+            and ut.branch='master'
+            order by timestamp desc
+        */
+        var sProDetailUTQuery = "select ut.tcid tcid, ut.pid projectId, p.name projectName, ut.passed passed, ut.failed failed, ut.assertion assertion, p.team teamId, t.name teamName, ut.timestamp timestamp from UT ut left join Project p on (ut.pid = p.pid ) left join Team t on (p.team = t.tid) where ut.pid = 'sap.support.sae'  and ut.branch='master' order by timestamp desc";
+        oDB.query(sProDetailUTQuery, function(oError, aResult){
             if(oError){
                 console.log("[Database error] - ", oError.message); //write header instead
                 return;
@@ -142,8 +218,93 @@ var oServer = HTTP.createServer(function(request, response){
         });
 
         //ITSet
+        /*
+            select 
+                it.tcid tcid,
+                it.pid projectId,
+                p.name projectName,
+                p.team teamId,
+                t.name teamName,
+                it.passed passed,
+                it.failed failed,
+                it.assertion assertion,
+                it.branch branch,
+                it.timestamp timestamp
+            from
+                (select 
+                    i.pid, 
+                    substring_index(group_concat(i.tcid), ',', -1) last_commit_id
+                from 
+                    (select 
+                        i.tcid,
+                        i.pid,
+                        i.timestamp
+                    from IT i
+                    where timestamp > 0
+                    and branch = 'master'
+                    order by i.pid asc, i.timestamp desc) i group by i.pid) l
+            left join IT it
+            on l.last_commit_id = it.tcid
+            left join Project p
+            on it.pid = p.pid
+            left join Team t
+            on p.team = t.tid
+            order by p.team desc, it.assertion desc
+        */
+        var sITQuery = "select  it.tcid tcid, it.pid projectId, p.name projectName, p.team teamId, t.name teamName, it.passed passed, it.failed failed, it.assertion assertion, it.branch branch, it.timestamp timestamp from (select  i.pid,  substring_index(group_concat(i.tcid), ',', -1) last_commit_id from  (select      i.tcid,     i.pid,     i.timestamp     from IT i     where timestamp > 0     and branch = 'master'     order by i.pid asc, i.timestamp desc) i group by i.pid) l left join IT it on l.last_commit_id = it.tcid left join Project p on it.pid = p.pid left join Team t on p.team = t.tid order by p.team desc, it.assertion desc";
+        oDB.query(sITQuery, function(oError, aResult){
+            if(oError){
+                console.log("[Database error] - ", oError.message); //write header instead
+                return;
+            }
+
+            if(sUrl.match("ITSet")){
+                response.end(JSON.stringify(aResult));
+            }else{
+                response.end(JSON.stringify({
+                    "key1": "I306293",
+                    "key2": "SAP-IUser"
+                }));
+            }
+        });
 
         //ProjectDetailITSet
+        /*
+        select 
+            it.tcid tcid,
+            it.pid projectId,
+            p.name projectName,
+            it.passed passed,
+            it.failed failed,
+            it.assertion assertion,
+            p.team teamId,
+            t.name teamName,
+            it.timestamp timestamp
+        from IT it
+        left join Project p
+        on (it.pid = p.pid )
+        left join Team t
+        on (p.team = t.tid)
+        where it.pid = 'sap.support.sae' 
+        and it.branch='master'
+        order by timestamp desc
+        */
+        var sProjectDetailITQuery = "select  it.tcid tcid, it.pid projectId, p.name projectName, it.passed passed, it.failed failed, it.assertion assertion, p.team teamId, t.name teamName, it.timestamp timestamp from IT it left join Project p on (it.pid = p.pid ) left join Team t on (p.team = t.tid) where it.pid = 'sap.support.sae'  and it.branch='master' order by timestamp desc";
+        oDB.query(sProjectDetailITQuery, function(oError, aResult){
+            if(oError){
+                console.log("[Database error] - ", oError.message); //write header instead
+                return;
+            }
+
+            if(sUrl.match("ProjectDetailITSet")){
+                response.end(JSON.stringify(aResult));
+            }else{
+                response.end(JSON.stringify({
+                    "key1": "I306293",
+                    "key2": "SAP-IUser"
+                }));
+            }
+        });
 
         //oDB.close();
         oDB.end();
