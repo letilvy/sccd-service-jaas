294aaef92c30386388502a9856e26b078f2589f7
Wanying Xie <wanying.xie@sap.com>
2018-03-01 14:45:10 +0800

    UTSet update

--- bin/service.js
+++ bin/service.js
@@ -8,7 +8,7 @@ var MySql = require("mysql");
 
 var oServer = HTTP.createServer(function(request, response){
     var sUrl = request.url;
-
+    //TODO: parse the url for entityset and parameter
     console.log(sUrl);
     
     response.writeHead(200, HTTP.STATUS_CODES[200], {
@@ -32,7 +32,83 @@ var oServer = HTTP.createServer(function(request, response){
 
         oDB.connect();
 
-        oDB.query("select u.tcid, p.name, u.passed, u.failed, u.assertion, u.skipped, u.timestamp from Project p, UT u where p.pid = u.pid order by timestamp", function(oError, aResult){
+        //projectId
+
+        //HomeSet: list all test cases of UT and IT
+        /*var queryStr = "";
+       
+        oDB.query(queryStr, function(oError, aResult){
+            if(oError){
+                console.log("[Database error] - ", oError.message); //write header instead
+                return;
+            }
+
+            if(sUrl.match("UITSet")){
+                response.end(JSON.stringify(aResult));
+            }else{
+                response.end(JSON.stringify({
+                    "key1": "I306293",
+                    "key2": "SAP-IUser"
+                }));
+            }
+        });*/
+
+        //UTSet: list the last commit UT data of all project
+        /*
+          select
+                ut.tcid,
+                ut.pid,
+                ut.name,
+                ut.team,
+                t.name,
+                ut.passed,
+                ut.failed,
+                ut.assertion,
+                ut.branch,
+                ut.timestamp
+            from
+                (select 
+                    ut.tcid,
+                    ut.pid,
+                    p.name,
+                    p.team,
+                    ut.passed,
+                    ut.failed,
+                    ut.assertion,
+                    ut.branch,
+                    ut.timestamp
+                from
+                    (select 
+                        ut.tcid,
+                        ut.pid,
+                        ut.passed,
+                        ut.failed,
+                        ut.assertion,
+                        ut.branch,
+                        ut.timestamp
+                    from
+                        (select 
+                            t.pid, 
+                            substring_index(group_concat(t.tcid), ',', -1) last_commit_id
+                        from 
+                            (select 
+                                u.tcid,
+                                u.pid,
+                                u.timestamp
+                            from UT u
+                            where timestamp > 0
+                            and branch = 'master'
+                            order by u.pid asc, timestamp desc) t group by t.pid) l
+                    left join UT ut
+                    on l.last_commit_id = ut.tcid) ut
+                left join Project p
+                on ut.pid = p.pid) ut
+            left join Team t
+            on ut.team = t.tid
+            order by ut.team, ut.assertion desc
+        */
+        var sUTQuery = "select ut.tcid, ut.pid, ut.name, ut.team, t.name, ut.passed, ut.failed, ut.assertion, ut.branch, ut.timestamp from (select ut.tcid, ut.pid, p.name, p.team, ut.passed, ut.failed, ut.assertion, ut.branch, ut.timestamp from (select ut.tcid, ut.pid, ut.passed, ut.failed, ut.assertion, ut.branch, ut.timestamp from (select t.pid, substring_index(group_concat(t.tcid), ',', -1) last_commit_id from (select u.tcid, u.pid, u.timestamp from UT u where timestamp > 0 and branch = 'master' order by u.pid asc, timestamp desc) t group by t.pid) l left join UT ut on l.last_commit_id = ut.tcid) ut left join Project p on ut.pid = p.pid) ut left join Team t on ut.team = t.tid order by ut.team, ut.assertion desc";
+        oDB.query(sUTQuery, function(oError, aResult){
             if(oError){
                 console.log("[Database error] - ", oError.message); //write header instead
                 return;
@@ -47,6 +123,27 @@ var oServer = HTTP.createServer(function(request, response){
                 }));
             }
         });
+
+        //ProjectDetailUTSet: list history UT data with specified project
+        oDB.query("select u.tcid, p.name, u.passed, u.failed, u.assertion, u.skipped, u.timestamp from Project p, UT u where p.pid = u.pid and p.pid='sap.support.expertchat' order by timestamp;", function(oError, aResult){
+            if(oError){
+                console.log("[Database error] - ", oError.message); //write header instead
+                return;
+            }
+
+            if(sUrl.match("ProjectDetailUTSet")){
+                response.end(JSON.stringify(aResult));
+            }else{
+                response.end(JSON.stringify({
+                    "key1": "I306293",
+                    "key2": "SAP-IUser"
+                }));
+            }
+        });
+
+        //ITSet
+
+        //ProjectDetailITSet
 
         //oDB.close();
         oDB.end();
