0152d56f127955a1e042586e5585a9e9b9cf3c94
Sherry Shao <sherry.shao01@sap.com>
2018-09-06 10:55:39 +0800

    Do test for add data to Hana DB

--- bin/main_test.js
+++ bin/main_test.js
@@ -2,8 +2,7 @@
 
 var Project = require("../lib/project");
 var Job = require("../lib/job");
-var DB = require("../lib/db");
-// var hanaDb = require("../lib/hanadb");
+var hanaDb = require("../lib/hanadb");
 var ToolKit = require("../lib/toolkit");
 var Argv = require("optimist").boolean("cors").argv;
 
@@ -36,8 +35,8 @@ if(Argv.h || Argv.help){
 
 var aArgv = process.argv.slice(2);
 
-var sWorkSpace = Argv.p || aArgv[0] || "./";
-//var sWorkSpace = "../data/workspace/B1_SMP_PUM"; //Use "../data/B1_SMP_PUM" for UI5 code debug purpose
+// var sWorkSpace = Argv.p || aArgv[0] || "./";
+var sWorkSpace = "../data/workspace/B1_SMP_PUM"; //Use "../data/B1_SMP_PUM" for UI5 code debug purpose
 // var sWorkSpace = "../data/workspace/BCD_ABAP_UT"; //Use "../data/BCD_ABAP_UT" for ABAP code debug purpose
 
 var oProject = new Project({
@@ -67,28 +66,15 @@ console.log("Get branch name: " + sBranch);
 
 //Save project information
 oProject.getProjectId().then(function(sProjectId){
-	var sProjectName = sProjectId.substr(sProjectId.lastIndexOf(".")+1);
-	console.log("sProjectId: " + sProjectId);
-	var oDB = new DB({ name: "sccd" });
-	oDB.insertNewUpdateExistDBItem(false, {
-		table: "Project",
-		keys: {
-			pid: sProjectId,
-			type: sProjectType
-		},
-		values: {
-			name: sProjectName
-		}
-	});
 
-// Sherry Test
-	// var oHana = new hanaDb();
-	// var oContent = {
-	// 	"pid": sProjectId,
-	// 	"type": sProjectType,
-	// 	"name": sProjectName
-	// }
-	// oHana.insertNewUpdateExistDBItem("ProjectsSet", oContent);
+	var oHana = new hanaDb();
+	var oContent = {
+		"ProjectId": sProjectId,
+		"Type": sProjectType,
+		"Name": sProjectName
+	}
+	// console.log(oContent);
+	oHana.post("ProjectSet", oContent);
 
 }).catch(function(sReason){
 	console.log("Save project information failed: " + sReason);
@@ -104,39 +90,31 @@ Promise.all([oProject.getProjectId(), oProject.getTestKpi(), oProject.getUTCover
 		var sTestType = Project.TestType[sTestTypeKey];
 
 		if(oKpi[sTestType].assertion){
-			console.log("Get " + sTestType + " test kpi: passed-" + oKpi[sTestType].passed + ", " +
-														"failed-" + oKpi[sTestType].failed + ", " +
-														"skipped-" + oKpi[sTestType].skipped + ", " +
-														"assertion-" + oKpi[sTestType].assertion +
-														(sTestType === Project.TestType.Unit ?
-															(", included stmt lines-" + oCoverage.Included.validLines + ", " + 
-															"included stmt coverage-" + oCoverage.Included.lineRate + ", " + 
-															"all stmt lines-" + oCoverage.All.validLines + ", " + 
-															"all stmt coverage-" + oCoverage.All.lineRate):"")
-														);
-			var oDB = new DB({ name: "sccd" });
-			oDB.insertNewUpdateExistDBItem(true, {
-				table: sTestType,
-				keys: {
-					pid: sProjectId,
-					type: sProjectType,
-    				tcid: undefined  //auto_increment DB field MUST BE passed as undefined
-    			},
-    			specialCondition: "timestamp LIKE '" + sTimestamp.slice(0,8) +"%'",
-				values: Object.assign({
-					branch: sBranch,
-					passed: oKpi[sTestType].passed,
-					failed: oKpi[sTestType].failed,
-					skipped: oKpi[sTestType].skipped,
-					assertion: oKpi[sTestType].assertion,
-					timestamp: sTimestamp
-    			}, (sTestType === Project.TestType.Unit ? {
-    				inclstmtlines: oCoverage.Included.validLines,
-    				inclstmtcover: oCoverage.Included.lineRate,
-    				allstmtlines: oCoverage.All.validLines,
-    				allstmtcover: oCoverage.All.lineRate
-    			}:{}))
-			});
+			var oHana = new hanaDb();
+			var oContent = Object.assign({
+				"Guid": undefined,
+				"Projectid": sProjectId,
+				"Type": sProjectType,
+				"Branch": sBranch,
+				"Passed": oKpi[sTestType].passed,
+				"Failed": oKpi[sTestType].failed,
+				"Skipped": oKpi[sTestType].skipped,
+				"Assertion": oKpi[sTestType].assertion,
+				"Timestamp": sTimestamp
+			}, (sTestType === Project.TestType.Unit ? {
+				"Inclstmtlines": oCoverage.Included.validLines,
+				"Inclstmtcover": oCoverage.Included.lineRate,
+				"Allstmtlines": oCoverage.All.validLines,
+				"Allstmtcover": oCoverage.All.lineRate
+			}:{}));
+			// console.log(sTestType + ":\n");
+			if (sTestType == "UT") {
+				// console.log(oContent);
+				oHana.post("UTSet", oContent);
+			} else if (sTestType == "IT") {
+				// console.log(oContent);
+				oHana.post("ITSet", oContent);
+			}
 		}
 	});
 }).catch(function(sReason){
@@ -163,28 +141,26 @@ oProject.getProjectId().then(function(sProjectId){
 	}
 
 	var sJobName = oJob.getJobBaseName();
-	console.log("Job: " + sProjectId + ", " + sProjectType + ", " + sTestType + ", " + sJobName + ", " + oJob.getLastBuildNumber() );
-	var oDB = new DB({ name: "sccd" });
-	oDB.insertNewUpdateExistDBItem(true, {
-		table: "Job",
-		keys: {
-			pid: sProjectId,
-			ptype: sProjectType,
-			ttype: sTestType
-		},
-		values: {
-			name: sJobName,
-			lastbuild: oJob.getLastBuildNumber()
-		}
-	});
+
+	var oHana = new hanaDb();
+	var oContent = {
+		"ProjectId": sProjectId,
+		"ProjectType": sProjectType,
+		"TestType": sTestType,
+		"Name": sJobName,
+		"LastBuild": oJob.getLastBuildNumber()
+	}
+	oHana.post("JobSet", oContent);
+
+
 }).catch(function(sReason){
 	console.log("Save job information failed: " + sReason);
 });
 
-if(!Argv.k && !Argv.keep){
-	// Job cleanup because of jenkins memory space limitation
-	if(sProjectType === Project.Type.FrontEnd){ //ABAP UT does not consume too much space. So we not do cleanup here. However daily job wil still cleanup its data
-		oJob.deleteJobNoKeepFiles();
-	}
-	oJob.deleteUIArtifact();
-}
+// if(!Argv.k && !Argv.keep){
+// 	// Job cleanup because of jenkins memory space limitation
+// 	if(sProjectType === Project.Type.FrontEnd){ //ABAP UT does not consume too much space. So we not do cleanup here. However daily job wil still cleanup its data
+// 		oJob.deleteJobNoKeepFiles();
+// 	}
+// 	oJob.deleteUIArtifact();
+// }
